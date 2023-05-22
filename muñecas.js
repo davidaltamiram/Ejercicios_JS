86ec4a9105bee06cae75520f3184e9b1eebedf66
David Altamira <davidaltamira19@gmail.com>
2023-05-22 17:45:59 -0600

    Ejercicio Jugueteria (peso muñecas y payasos)

---
+++ muñecas.js
@@ -0,0 +1,8 @@
+console.log("Aqui inicia el ejercicio jugueteria");
+payaso = 112;
+muñeca = 75;
+numero_de_muñecas = 14;
+numero_de_payasos = 27;
+peso_paquete = (numero_de_muñecas*muñeca) + (numero_de_payasos*payaso);
+
+console.log("El peso total del paquete es: " + peso_paquete);
