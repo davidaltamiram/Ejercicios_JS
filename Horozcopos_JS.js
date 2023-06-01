dd13a33c6ff3701862333565a348e2ec47865052
David Altamira <davidaltamira19@gmail.com>
2023-05-31 20:23:25 -0600

    Ejercicio Horozcopos con frase de nemo

---
+++ Horozcopos_JS.js
@@ -0,0 +1,42 @@
+let signo = prompt("Ingresa tu signo zodiacal para obtener tu horoscopo del dia de hoy hermano: ");
+
+switch(signo){
+    case "aries":
+        alert("Confía en mí, ¿sí? Los amigos hacen eso.");
+        break;
+    case "tauro":
+        alert("Una pregunta Dory: ¿tienes hambre? Te vas a cenar mis burbujas.");
+        break;
+    case "geminis":
+        alert("Ríndete viejo, esto es evolución, nací para ser veloz.");
+        break;
+    case "cancer":
+        alert("Oye, para tu tren, hay que dejar que Chiqui lo haga solo.");
+        break;
+    case "leo":
+        alert("Es increíble Medusin, cuando los chicos son huevos los dejamos en la playa, salir del cascarón y luego curicachu  buscan el camino de vuelta al viejo azul.");
+        break;
+    case "virgo":
+        alert("Tengo que salir, debo encontrar a mi hijo, tengo que decirle qué edad tienen las tortugas.");
+        break;
+    case "libra":
+        alert("Sí oye, P. Sherman, calle, P., Sher, no se me olvido, lo tengo, lo sé porque lo siento cuando te veo y cuando te veo estoy en mi hogar.");
+        break;
+    case "escorpio":
+        alert("No sé dónde estoy. No sé que pasa, creo que he perdido a alguien pero no… ¡no me acuerdo! Y.. ¡y necesito acordarme!");
+        break;
+    case "sagitario":
+        alert("Los peces son amigos, no comida (el tiburón vegetariano).");
+        break;
+    case "capricornio":
+        alert("!Oh, Cielos! Nemo está nadando!");
+        break;
+    case "acuario":
+        alert("tu di como nemo, 'Oye, para tu tren, hay que dejar que Chiqui lo haga solo.' se feliz");
+        break;
+    case "picis":
+        alert("te dejo esta frase de disney: 'Sherman, 42 wallaby way, Sydney. ¡Lo recordé! ' ");
+        break;
+    default:
+        alert("Pues ese no es un signo zodiacal mi ciela!" );
+}
